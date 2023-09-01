765bfed9501b23300eb5667bb8a67996ad8498aa
Erik <erik.raboczki98@gmail.com>
2023-09-01 12:53:58 +0200

    Ok, I progressed with the task but I think I will leave it there until the Live Coding

--- index.js
+++ index.js
@@ -6,11 +6,96 @@ const app = express();
 app.use(express.json());
 
 
+app.get("/api/v1/getPeopleData", (req, res) => {
+    fs.readFile('./data/people.json', 'utf-8', (err, data) => {
+        if (err) res.status(500).json({ message: "Upss, something went wrong, please try again later!" });
+        const items = JSON.parse(data)
+        res.json(items)
+    })
+})
 
+app.post("/api/v1/addNewPerson", (req, res) => {
+    fs.readFile('./data/people.json', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+        }
 
+        const allData = JSON.parse(data)
+        const newData = {
+            name: req.body.name,
+            age: req.body.age
+        }
+        allData.push(newData)
 
+        fs.writeFile('./data/people.json', JSON.stringify(allData), (err) => {
+            if (err) {
+                return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+            }
 
+            return res.json({ message: "Hurray the data have been saved!" })
+        })
+    })
+})
 
+app.put("/api/v1/replacePerson/:name", (req, res) => {
+    fs.readFile('./data/people.json', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+        }
+
+        const allData = JSON.parse(data)
+        const selectedPerson = allData.find(nextData => nextData.name === req.params.name)
+        selectedPerson.name = req.body.name
+        selectedPerson.age = req.body.age
+
+        fs.writeFile('./data/people.json', JSON.stringify(allData), (err) => {
+            if (err) {
+                return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+            }
+
+            return res.json({ message: "Hurray the data have been updated!" })
+        })
+    })
+})
+
+app.patch("/api/v1/changeName/:name", (req, res) => {
+    fs.readFile('./data/people.json', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+        }
+
+        const allData = JSON.parse(data)
+        const selectedPerson = allData.find(nextData => nextData.name === req.params.name)
+        selectedPerson.name = req.body.name
+
+        fs.writeFile('./data/people.json', JSON.stringify(allData), (err) => {
+            if (err) {
+                return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+            }
+
+            return res.json({ message: "Hurray the data have been saved!" })
+        })
+    })
+})
+
+app.delete("/api/v1/deletePerson/:name", (req, res) => {
+    fs.readFile('./data/people.json', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+        }
+
+        const allData = JSON.parse(data)
+        const newArray = allData.filter(nextData => nextData.name !== req.params.name)
+
+        fs.writeFile('./data/people.json', JSON.stringify(newArray), (err) => {
+            if (err) {
+                return res.status(500).json({ message: "Upss, something went wrong, please try again later!" }) 
+            }
+
+            return res.json({ message: "Hurray the data have been deleted!" })
+        })
+    })
+})
 
 
 app.get("/", (req, res, next) => {
